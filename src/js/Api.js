5b73bea5d5b3079da64ad0d0a67719473df21a75
Pola <polaskibinska@gmail.com>
2020-10-11 13:23:04 +0200

    Create fetchWeather method in Api.js

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,7 +1,18 @@
+const KEY = "f9a1e044fc37ed8a5047d51e6d8a3ed8";
+
 class Api {
-    fetchData() {
-        // TO DO
+  static async fetchWeather(city, option) {
+    try {
+      let weather = await fetch(
+        `https://api.openweathermap.org/data/2.5/${option}?q=${city}&APPID=${KEY}&units=metric`
+      );
+      weather = await weather.json();
+      console.log(weather);
+      return weather;
+    } catch (err) {
+      console.log(err);
     }
+  }
 }
 
 export default Api;
