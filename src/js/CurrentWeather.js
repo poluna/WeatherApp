7edc8ae7d001d12b8aa62a0cf94cc07b55d2d9af
Pola <polaskibinska@gmail.com>
2020-10-27 18:05:35 +0100

    Add a change in the webstie appearance depending on the weather

--- src/js/CurrentWeather.js
+++ src/js/CurrentWeather.js
@@ -25,14 +25,30 @@ class CurrentWeather {
       ".pressure p"
     ).innerHTML = `${data.main.pressure} hPa`;
     document.querySelector(".wind p").innerHTML = `${data.wind.speed} m/s`;
+    console.log(data.weather[0].icon[2]);
+    let backgroundColor, fontColor;
+    console.log(data.weather[0].icon[2] === "n");
+    if (data.weather[0].icon[2] === "n") {
+      backgroundColor =
+        "linear-gradient(to right top, #091b36, #0a2953, #0b3972, #0d4892, #1358b3)";
+      fontColor = "rgb(245, 245, 245)";
+    } else {
+      backgroundColor =
+        "linear-gradient(to right top, #265e89, #3981a9, #51a5c7, #6fcbe4, #91f1ff)";
+      fontColor = "rgb(0, 0, 0)";
+    }
+    document.querySelector("body").style.backgroundImage = backgroundColor;
+    document.querySelector(".main-info").style.color = fontColor;
   }
 
   render() {
     this.element.innerHTML = `
-            <img src="./img/icons/unknown.png"/>
-            <h2 class="temperature">- °C</h2>
-            <h2 class="description">-</h2>
-            <h3 class="location">-</h3>
+            <div class="main-info">
+              <img src="./img/icons/unknown.png"/>
+              <h2 class="temperature">- °C</h2>
+              <h2 class="description">-</h2>
+              <h3 class="location">-</h3>
+            </div>
             <a href="#forecast">See forecast for the next days ⌄</a>
         `;
     this.detailspanel.render(this.element);
